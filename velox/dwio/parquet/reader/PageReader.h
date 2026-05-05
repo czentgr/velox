ee8e11d57bad8b4dd00e43da81549830cd44f1bb
peterenescu <32805429+peterenescu@users.noreply.github.com>
2026-05-05 14:08:55 -0700

    build: Use FBThrift instead of Apache Thrift (#16019) (#16019)
    Summary:
    Removes external Thrift dependency primarily used in Parquet as noted by [https://github.com/facebookincubator/velox/issues/13175](https://github.com/facebookincubator/velox/issues/13175).
    
    *Issues noted below for which we'd like to move to FBThrift from Apache Thrift.*
    
    FBThrift and Apache Thrift has many incompatible API changes:
    
    * `XXX.__isset.YYY` -> `XXX.YYY().has_value()` (or `bool(XXX.YYY())`): The latter implicit `bool(...)` syntax is used in this PR
    * `XXX.__isset.YYY = true` -> `XXX.YYY().ensure()`
    * `XXX.YYY` -> `XXX.YYY().value()` (or `*XXX.YYY()`): The latter `*...` syntax is used in this PR
    * `XXX.__set_YYY(value)` -> `XXX.YYY() = value`
    * `thrift::ThriftTransport`/`TCompactProcotolT` -> `folly::IOBuf`/`CompactProtocolReader`
    * FBThrift doesn't support `optional` and default value that is used by `DataPageHeaderV2.is_compressed`: We need to use the default value manually in our code
    * FBThrift raises an exception when we access to an optional field that isn't set (Apache Thrift doesn't have the check)
    
    There are many changes in this PR but there is no logic change and no refactoring. This just converts API usage for FBThrift.
    
    (Copy of [https://github.com/facebookincubator/velox/issues/14942](https://github.com/facebookincubator/velox/pull/14942) from kou due to some push permission issues.)
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/16019
    
    Differential Revision: D90723225
    
    Pulled By: peterenescu

--- velox/dwio/parquet/reader/PageReader.h
+++ velox/dwio/parquet/reader/PageReader.h
@@ -216,6 +216,19 @@ class PageReader {
   // 'hasChunkRepDefs_' is false.
   void readPageDefLevels();
 
+  // Updates bufferStart_ and bufferEnd_ based on deserialization result.
+  // Handles both refiller and non-refiller cases.
+  void updateBufferPointersAfterDeserialization(
+      const thrift::DeserializeResult& result);
+
+  static inline const char* toCharPtr(const uint8_t* ptr) {
+    return reinterpret_cast<const char*>(ptr);
+  }
+
+  static inline const char* toCharPtr(const void* ptr) {
+    return static_cast<const char*>(ptr);
+  }
+
   // Returns a pointer to contiguous space for the next 'size' bytes
   // from current position. Copies data into 'copy' if the range
   // straddles buffers. Allocates or resizes 'copy' as needed.
@@ -395,6 +408,9 @@ class PageReader {
   const int64_t chunkSize_;
   const char* bufferStart_{nullptr};
   const char* bufferEnd_{nullptr};
+  // Holds the buffer from the last Thrift deserialization to keep
+  // deserialized data pointers valid
+  std::unique_ptr<folly::IOBuf> thriftBuffer_;
   BufferPtr tempNulls_;
   BufferPtr nullsInReadRange_;
   BufferPtr multiPageNulls_;
@@ -439,7 +455,7 @@ class PageReader {
   raw_vector<uint64_t> leafNulls_;
 
   // Encoding of current page.
-  thrift::Encoding::type encoding_;
+  thrift::Encoding encoding_;
 
   // Row number of first value in current page from start of ColumnChunk.
   int64_t rowOfPage_{0};
@@ -463,7 +479,7 @@ class PageReader {
 
   // Dictionary contents.
   dwio::common::DictionaryValues dictionary_;
-  thrift::Encoding::type dictionaryEncoding_;
+  thrift::Encoding dictionaryEncoding_;
 
   // Offset of current page's header from start of ColumnChunk.
   uint64_t pageStart_{0};
